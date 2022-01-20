b97a722fa3333aa8bdd643812c84b2ef385c2165
Gustavo Marques <gustavomarques.gml@gmail.com>
2022-01-20 14:02:33 -0300

    Addition of process_message script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-
-sh sync.sh
-sh judge.sh OlaMundo.c c hello_world
+while :
+do
+	echo "Syncronizing with S3"
+    ./sync.sh
+    echo "Searching for messages"
+    result=$(./process_message.sh)
+    result_code=$?
+    if [ $result_code -eq 0 ]; then
+        echo "Message processed successfully"
+        echo "Result is $result"
+    elif [ $result_code -eq 2 ]; then
+        echo "No messages to process - sleeping"
+        sleep 10
+    else
+        echo "Message not processed"
+        echo "Result is $result"
+    fi
+done
--- process_message.sh
+++ process_message.sh
@@ -4,8 +4,7 @@ MAX_MESSAGES=1
 response=$(aws sqs receive-message --queue-url $QUEUE_URL --max-number-of-messages $MAX_MESSAGES)
 raw_message=$(echo $response | jq -r '.Messages[0]')
 if [ -z "$raw_message" ]; then
-    echo "No messages"
-    exit 0
+    exit 2
 fi
 
 message=$(echo $raw_message | jq -r '.Body')
@@ -13,11 +12,12 @@ file_key=$(echo $message | jq -r '.FileKey')
 problem_id=$(echo $message | jq -r '.ProblemId')
 language=$(echo $message | jq -r '.Language')
 
-echo "Processing file $file_key"
 result=$(./judge.sh $file_key $language $problem_id)
+echo $result
+
 if [ $? -eq 0 ]; then
-    echo "OK"
     aws sqs delete-message --queue-url $QUEUE_URL --receipt-handle $(echo $raw_message | jq -r '.ReceiptHandle')
+    exit 0
 else
-    echo "FAIL - $result"
+    exit 1
 fi
